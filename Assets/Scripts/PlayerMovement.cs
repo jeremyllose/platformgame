b66c6c10ccab3bf8a050852dbb2e28f42ee8ad6b
jeremyllose <jeremy224.llose@gmail.com>
2025-02-14 17:30:19 +0800

    save just in case

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().linearVelocity = new Vector3(0,14,0);
+        }
+    }
+}
